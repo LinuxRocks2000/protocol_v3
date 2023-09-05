671d757253942623c5466b34a022db13ffaa4e93
Lazy Hippopotamus <lazy@hippo.com>
2023-09-05 15:53:50 -0400

    update description and stuff

---
+++ protocol_v3_macro/target/package/protocol_v3_macro-0.1.0/target/debug/build/proc-macro2-f499e9a6036036ac/build_script_build-f499e9a6036036ac.d
@@ -0,0 +1,5 @@
+/home/awesome/desk/programs/rust/protocol_v3/protocol_v3_macro/target/package/protocol_v3_macro-0.1.0/target/debug/build/proc-macro2-f499e9a6036036ac/build_script_build-f499e9a6036036ac: /home/awesome/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.66/build.rs
+
+/home/awesome/desk/programs/rust/protocol_v3/protocol_v3_macro/target/package/protocol_v3_macro-0.1.0/target/debug/build/proc-macro2-f499e9a6036036ac/build_script_build-f499e9a6036036ac.d: /home/awesome/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.66/build.rs
+
+/home/awesome/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.66/build.rs:
